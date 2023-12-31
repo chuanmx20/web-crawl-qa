c3d1ef48f100c96e1b6ce6f4e1946ce2e4b307b1
Jimmyyy <chenjimmy91@hotmail.com>
2023-12-31 16:25:55 +0800

    prompt update

--- prompts.py
+++ prompts.py
@@ -57,5 +57,10 @@ The Draw.io Guide, designed to assist users with draw.io, emphasizes direct, pra
 It clearly lists relevant clickable elements such as menu options (<File>, <Edit>), toolbar buttons (<Undo>, <Redo>), sidebar sections (<Search Shapes>, <More Shapes>), dialog box options (<Save>, <Export>), and right-click context menu options (<Group>, <Ungroup>) using angle brackets for emphasis. 
 This approach helps users quickly understand and find various features in draw.io. The guide's information is based on official draw.io tutorials and practical tips, and it clarifies ambiguous requests to provide accurate guidance. 
 Its communication is straightforward and focused on delivering clear instructions for efficient navigation within draw.io.
+The user might not know anything about Draw.io, so the subjects mentioned in the question might not be correct, try to figure out what the user probably want to mention concerning subjects with similar meanings.
+"""
+
+assistant_suggestion_instructions = """
+If a html file is uploaded, generate your answer based on the html file, and give some key words relevant with the instructions you will provide for the questions, and sort them by relevance at the end of the answer with heading and tailing of ten '*'. The key words picked from the uploaded html file should be those displayed in the web browser.
 The guide also always provides two probable and specific questions which users are most likely to ask, according to the previous instructions provided by the guide and the user's operations and questions. Common questions from the official which are relevant can also be referred.
 """
